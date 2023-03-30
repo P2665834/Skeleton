fc47d3576ce5820d3f51230db35ff9bf5c76c5b9
Fayaz Ughradar <P2665834@my365.dmu.ac.uk>
2023-03-30 16:07:27 +0100

    Created validation

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -121,6 +121,64 @@ namespace ClassLibrary
                 return false;
             }
         }
-        
+
+        public string Valid(string SupplierName, string ProductName, string QuantityOfProducts, string UnitPrice, string DatePurchased)
+        {
+            String Error = "";
+            DateTime DateTemp;
+            if (SupplierName.Length == 0)
+            {
+                Error = Error + "The Supplier Name may not be blank : ";
+            }
+            if (SupplierName.Length > 50)
+            {
+                Error = Error + "The supplier name must be less than 50 characters : ";
+            }
+            if (ProductName.Length == 0)
+            {
+                Error = Error + "The product name may not be blank : ";
+            }
+            if (ProductName.Length > 50)
+            {
+                Error = Error + "The product name must be less than 50 characters : ";
+            }
+            if (QuantityOfProducts.Length == 0)
+            {
+                Error = Error + "The quantity of products may not be blank : ";
+            }
+            if (QuantityOfProducts.Length > 32)
+            {
+                Error = Error + "The quantity of products must be less than 32 characters : ";
+            }
+            if (UnitPrice.Length > 53)
+            {
+                Error = Error + "The unit price must be less than 53 characters : ";
+            }
+            if (UnitPrice.Length == 0)
+            {
+                Error = Error + "The unit price may not be blank : ";
+            }
+
+            try
+            {
+                DateTemp = Convert.ToDateTime(DatePurchased);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the past : ";
+                }
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                Error = Error + "The date was not a valid date : ";
+            }
+
+            return Error;
+
+        }
+
     }
 }
--- Testing2/tstSupplier.cs
+++ Testing2/tstSupplier.cs
@@ -7,6 +7,12 @@ namespace Testing2
     [TestClass]
     public class tstSupplier
     {
+        string SupplierName = "hp";
+        string ProductName = "15s-fq2024na";
+        string QuantityOfProducts = "25";
+        string UnitPrice = "200";
+        string DatePurchased = DateTime.Now.Date.ToString();
+    
         [TestMethod]
         public void InstanceOK()
 
@@ -197,6 +203,382 @@ namespace Testing2
             Assert.IsTrue(OK);
         }
 
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void SupplierNameMinLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string SupplierName = "";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void SupplierNameMin()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string SupplierName = "a";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void SupplierNameMinPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string SupplierName = "aa";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void SupplierNameMaxLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string SupplierName = "";
+            SupplierName = SupplierName.PadRight(49, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void SupplierNameMax()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string SupplierName = "";
+            SupplierName = SupplierName.PadRight(50, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void SupplierNameMaxPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string SupplierName = "";
+            SupplierName = SupplierName.PadRight(51, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void SupplierNameMid()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string SupplierName = "";
+            SupplierName = SupplierName.PadRight(25, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ProductNameMinLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string ProductName = "";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ProductNameMin()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string ProductName = "a";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ProductNameMinPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string ProductName = "aa";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ProductNameMaxLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string ProductName = "";
+            ProductName = ProductName.PadRight(49, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ProductNameMax()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string ProductName = "";
+            ProductName = ProductName.PadRight(50, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ProductNameMaxPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string ProductName = "";
+            ProductName = ProductName.PadRight(51, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void ProductNameMid()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string ProductName = "";
+            ProductName = ProductName.PadRight(25, 'a');
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DatePurchasedExtremeMin()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(-100);
+            string DatePurchased = TestDate.ToString();
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DatePurchasedMinLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(-1);
+            string DatePurchased = TestDate.ToString();
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DatePurchasedMin()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            string DatePurchased = TestDate.ToString();
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DatePurchasedMinPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddDays(1);
+            string DatePurchased = TestDate.ToString();
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DatePurchasedExtremeMax()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(100);
+            string DatePurchased = TestDate.ToString();
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void UnitPriceMinLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string UnitPrice = "";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void UnitPriceMin()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string UnitPrice = "";
+            UnitPrice = UnitPrice.PadRight(0);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void UnitPriceMinPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string UnitPrice = "";
+            UnitPrice = UnitPrice.PadRight(1);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void UnitPriceMaxLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string UnitPrice = "";
+            UnitPrice = UnitPrice.PadRight(52);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void UnitPriceMax()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string UnitPrice = "";
+            UnitPrice = UnitPrice.PadRight(53);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void UnitPriceMaxPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string UnitPrice = "";
+            UnitPrice = UnitPrice.PadRight(54);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void UnitPriceMid()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string UnitPrice = "";
+            UnitPrice = UnitPrice.PadRight(26);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void QuantityOfProductsMinLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string QuantityOfProducts = "";
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void QuantityOfProductsMin()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string QuantityOfProducts = "";
+            QuantityOfProducts = QuantityOfProducts.PadRight(0);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void QuantityOfProductsMinPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string QuantityOfProducts = "";
+            QuantityOfProducts = QuantityOfProducts.PadRight(1);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void QuantityOfProductsMaxLessOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string QuantityOfProducts = "";
+            QuantityOfProducts = QuantityOfProducts.PadRight(31);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void QuantityOfProductsMax()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string QuantityOfProducts = "";
+            QuantityOfProducts = QuantityOfProducts.PadRight(32);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void QuantityOfProductsMaxPlusOne()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string QuantityOfProducts = "";
+            QuantityOfProducts = QuantityOfProducts.PadRight(33);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void QuantityOfProductsMid()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            String Error = "";
+            string QuantityOfProducts = "";
+            QuantityOfProducts = QuantityOfProducts.PadRight(16);
+            Error = ASupplier.Valid(SupplierName, ProductName, QuantityOfProducts, UnitPrice, DatePurchased);
+            Assert.AreEqual(Error, "");
+        }
+
+
 
     }
+
 }
