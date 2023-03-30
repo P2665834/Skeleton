b855be620061c4d80c47bb761521d54aec0e9cc6
Fayaz Ughradar <P2665834@my365.dmu.ac.uk>
2023-03-30 17:43:17 +0100

    Added validation to the presentation layer

--- AdminSystem/StocksDataEntry.aspx.cs
+++ AdminSystem/StocksDataEntry.aspx.cs
@@ -15,16 +15,31 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-        clsStock AStock = new clsStock();
-        AStock.ProductName = txtProductName.Text;
-        AStock.ProductID = Convert.ToInt32(txtProductID.Text);
-        AStock.DateOfPurchase = Convert.ToDateTime(txtDateOfPurchase.Text);
-        AStock.StockQuantity = Convert.ToInt32(txtStockQuantity.Text);
-        AStock.UnitPrice = (float)Convert.ToDouble(txtUnitPrice.Text);
-        AStock.Available = Convert.ToBoolean(chkAvailable.Checked);
 
-        Session["AStock"] = AStock;
-        Response.Redirect("StocksViewer.aspx");
+        clsStock AStock = new clsStock();
+
+        string ProductName = txtProductName.Text;
+        string QuantityOfProducts = txtStockQuantity.Text;
+        string UnitPrice = txtUnitPrice.Text;
+        string DateOfPurchase = txtDateOfPurchase.Text;
+        string Error = "";
+        Error = AStock.Valid(ProductName, QuantityOfProducts, UnitPrice, DateOfPurchase);
+        if (Error == "")
+        {
+            AStock.ProductName = ProductName;
+            AStock.StockQuantity = Convert.ToInt32(UnitPrice);
+            AStock.UnitPrice = (float)Convert.ToDouble(UnitPrice);
+            AStock.DateOfPurchase = Convert.ToDateTime(DateOfPurchase);
+
+            Session["AStock"] = AStock;
+            Response.Write("StocksViewer.aspx");
+        }
+        else
+        {
+            lblError.Text = Error;
+        }
+
+
     }
 
     protected void btnFind_Click(object sender, EventArgs e)
