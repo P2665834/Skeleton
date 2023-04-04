cf19df0965a5e148a06bf47421d221b6f2013088
Fayaz Ughradar <P2665834@my365.dmu.ac.uk>
2023-04-04 16:44:40 +0100

    Created class collection

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+
+
+    {
+        List<clsStock> mStockList = new List<clsStock>();
+
+        public clsStockCollection()
+        {
+            clsStock TestItem = new clsStock();
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "15s - fq2024na";
+            TestItem.StockQuantity = 25;
+            TestItem.UnitPrice = 200;
+            TestItem.DateOfPurchase = DateTime.Now.Date;
+            TestItem.Available = true;
+
+            mStockList.Add(TestItem);
+            TestItem = new clsStock();
+            TestItem.ProductID = 2;
+            TestItem.ProductName = "Asus Chromebook CM14";
+            TestItem.StockQuantity = 29;
+            TestItem.UnitPrice = 300;
+            TestItem.DateOfPurchase = DateTime.Now.Date;
+            TestItem.Available = true;
+            mStockList.Add(TestItem);
+
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblStock_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsStock AStock = new clsStock();
+                AStock.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductID"]);
+                AStock.ProductName = Convert.ToString(DB.DataTable.Rows[Index]["ProductName"]);
+                AStock.StockQuantity = Convert.ToInt32(DB.DataTable.Rows[Index]["Quantity"]);
+                AStock.UnitPrice = (float)Convert.ToDouble(DB.DataTable.Rows[Index]["UnitPrice"]);
+                AStock.DateOfPurchase = Convert.ToDateTime(DB.DataTable.Rows[Index]["DatePurchased"]);
+                AStock.Available = Convert.ToBoolean(DB.DataTable.Rows[Index]["Available"]);
+                mStockList.Add(AStock);
+                Index++;
+            }
+        }
+
+
+
+
+
+        public List<clsStock> StockList
+        {
+            get
+            {
+                return mStockList;
+            }
+            set
+            {
+                mStockList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mStockList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsStock ThisStock { get; set; }
+    }
+}
---
+++ Testing3/tstStockCollection.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System.Collections.Generic;
+using System;
+
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+            Assert.IsNotNull(AllStock);
+
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "HP 15s-fq2024na";
+            TestItem.StockQuantity = 24;
+            TestItem.UnitPrice = 250;
+            TestItem.DateOfPurchase = DateTime.Now.Date;
+            TestItem.Available = true;
+            TestList.Add(TestItem);
+            AllStock.StockList = TestList;
+            Assert.AreEqual(AllStock.StockList, TestList);
+        }
+
+
+        [TestMethod]
+        public void ThisStockPropertyOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+            clsStock TestStock = new clsStock();
+            TestStock.ProductID = 1;
+            TestStock.ProductName = "HP 15s-fq2024na";
+            TestStock.StockQuantity = 24;
+            TestStock.UnitPrice = 250;
+            TestStock.DateOfPurchase = DateTime.Now.Date;
+            TestStock.Available = true;
+            AllStock.ThisStock = TestStock;
+            Assert.AreEqual(AllStock.ThisStock, TestStock);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "HP 15s-fq2024na";
+            TestItem.StockQuantity = 24;
+            TestItem.UnitPrice = 250;
+            TestItem.DateOfPurchase = DateTime.Now.Date;
+            TestItem.Available = true;
+            TestList.Add(TestItem);
+            AllStock.StockList = TestList;
+            Assert.AreEqual(AllStock.Count, TestList.Count);
+        }
+
+
+
+    }
+}
