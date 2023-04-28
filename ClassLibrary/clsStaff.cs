9c55e46a9410d84bc94fdfb54a2852479602bf11
EisharX <124702243+EisharX@users.noreply.github.com>
2023-04-28 10:48:11 +0100

    Completed Class files added

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+
+        private Int32 mEmployeeID;
+        private String mEmployeeFullName;
+        private String mEmployeePosition;
+        private float mSalary;
+        private DateTime mDateOfEmployment;
+        private Boolean mCurrentlyWorking;
+
+
+        public int EmployeeID
+        {
+            get
+            {
+                return mEmployeeID;
+            }
+            set
+            {
+                mEmployeeID = value;
+            }
+        }
+
+        public string EmployeeFullName
+        {
+            get
+            {
+                return mEmployeeFullName;
+            }
+            set
+            {
+                mEmployeeFullName = value;
+            }
+        }
+
+        public string EmployeePosition
+        {
+            get
+            {
+                return mEmployeePosition;
+            }
+            set
+            {
+                mEmployeePosition = value;
+            }
+        }
+
+        public DateTime DateOfEmployment
+        {
+            get
+            {
+                return mDateOfEmployment;
+            }
+            set
+            {
+                mDateOfEmployment = value;
+            }
+        }
+
+       
+        public float Salary
+        {
+            get
+            {
+                return mSalary;
+            }
+            set
+            {
+                mSalary = value;
+            }
+        }
+        public bool  CurrentlyWorking
+        {
+            get
+            {
+                return mCurrentlyWorking;
+            }
+            set
+            {
+                mCurrentlyWorking = value;
+            }
+        }
+
+
+
+        public bool Find(int EmployeeID)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@EmployeeID", EmployeeID);
+            DB.Execute("sproc_tblStaff_FilterByStaffID");
+            if (DB.Count == 1)
+            {
+                mEmployeeID = Convert.ToInt32(DB.DataTable.Rows[0]["EmployeeID"]);
+                mEmployeeFullName = Convert.ToString(DB.DataTable.Rows[0]["EmployeeFullName"]);
+                mEmployeePosition = Convert.ToString(DB.DataTable.Rows[0]["EmployeePosition"]);
+                mDateOfEmployment = Convert.ToDateTime(DB.DataTable.Rows[0]["DateOfEmployment"]);
+                mSalary = (float)Convert.ToDouble(DB.DataTable.Rows[0]["Salary"]);
+                mCurrentlyWorking = Convert.ToBoolean(DB.DataTable.Rows[0]["CurrentlyWorking"]);
+
+
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Valid(string EmployeeFullName, string EmployeePosition,  string DateOfEmployment, string Salary)
+        {
+            String Error = "";
+            DateTime DateTemp;
+            if (EmployeeFullName.Length == 0)
+            {
+                Error = Error + "The employee full Name cannot not be blank : ";
+            }
+            if (EmployeeFullName.Length > 50)
+            {
+                Error = Error + "The employee full name must be less than 50 characters : ";
+            }
+            if (EmployeePosition.Length == 0)
+            {
+                Error = Error + "The employee's postion cannot not be blank : ";
+            }
+            if (EmployeePosition.Length > 20)
+            {
+                Error = Error + "The employee's position name must be less than 20 characters : ";
+            }
+            
+            if (Salary.Length > 53)
+            {
+                Error = Error + "The employee's salary must be less than 53 characters : ";
+            }
+            if (Salary.Length == 0)
+            {
+                Error = Error + "The the employee's salary may not be blank : ";
+            }
+
+            try
+            {
+                DateTemp = Convert.ToDateTime(DateOfEmployment);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the past : ";
+                }
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    Error = Error + "The date cannot be in the future : ";
+                }
+            }
+            catch
+            {
+                Error = Error + "The date was not a valid date : ";
+            }
+
+            return Error;
+
+        }
+
+    }
+}
---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStaffCollection
+    {
+
+        List<clsStaff> mEmployeeList = new List<clsStaff>();
+        clsStaff mThisEmployee = new clsStaff();
+
+        public clsStaffCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblOrder_SelectAll");
+            PopulateArray(DB);
+        }
+
+
+
+
+
+        public List<clsStaff> EmployeeList
+        {
+            get
+            {
+                return mEmployeeList;
+            }
+            set
+            {
+                mEmployeeList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mEmployeeList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsStaff ThisEmployee
+        {
+            get
+            {
+                return mThisEmployee;
+            }
+
+            set
+            {
+                mThisEmployee = value;
+            }
+        }
+
+        public int Add()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@EmployeeFullName", mThisEmployee.EmployeeFullName);
+            DB.AddParameter("@EmployeePosition", mThisEmployee.EmployeePosition);
+            DB.AddParameter("@Salary", mThisEmployee.UnitPrice);
+            DB.AddParameter("@DateOfEmployment", mThisEmployee.DateOfEmployment);
+            DB.AddParameter("@CurrentlyWorking", mThisEmployee.CurrentlyWorking);
+            return DB.Execute("sproc_tblStaff_Insert");
+        }
+    
+
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@EmployeeID", mThisEmployee.EmployeeID);
+            DB.AddParameter("@EmployeeFullName", mThisEmployee.EmployeeFullName);
+            DB.AddParameter("@EmployeePosition", mThisEmployee.EmployeePosition);
+            DB.AddParameter("@Salary", mThisEmployee.UnitPrice);
+            DB.AddParameter("@DateOfEmployment", mThisEmployee.DateOfEmployment);
+            DB.AddParameter("@CurrentlyWorking", mThisEmployee.CurrentlyWorking);
+            DB.Execute("sproc_tblStaff_Update");
+
+
+
+        }
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@EmployeeID", mThisEmployee.EmployeeID);
+            DB.Execute("sproc_tblStaff_Delete");
+        }
+
+        public void ReportByEmployeeFullName(string EmployeeFullName)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@EmployeeFullName", EmployeeFullName);
+            DB.Execute("sproc_tblStaff_FilterByEmployeeFullName");
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            RecordCount = DB.Count;
+            mEmployeeList = new List<clsStaff>();
+            while (Index < RecordCount)
+            {
+
+                clsStaff aMemberOfstaff = new clsStaff();
+                aMemberOfstaff.EmployeeID = Convert.ToInt32(DB.DataTable.Rows[Index]["EmployeeID"]);
+                aMemberOfstaff.EmployeeFullName = Convert.ToString(DB.DataTable.Rows[Index]["EmployeeFullName]);
+                aMemberOfstaff.EmployeePosition = Convert.ToString(DB.DataTable.Rows[Index]["EmployeePosition"]);
+                aMemberOfstaff.DateOfEmployment = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOfEmployment"]);
+                aMemberOfstaff.Salary= (float)Convert.ToDouble(DB.DataTable.Rows[Index]["Salary"]);
+                aMemberOfstaff.CurrentlyWorking = Convert.ToBoolean(DB.DataTable.Rows[Index]["Available"]);
+                mEmployeeList.Add(aMemberOfstaff);
+                Index++;
+            }
+        }
+    }
+}
